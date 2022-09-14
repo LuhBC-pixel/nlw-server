251a928f93d46edddcc12fe239349f4e11691bee
Luisa Boina Coltro <bc.luisa@yahoo.com.br>
2022-09-13 22:24:47 -0300

    aula 3

---
+++ src/utils/convert-minuto-to-hour.ts
@@ -0,0 +1,9 @@
+export function convertMinuteToHout(minutes: number) {
+  const hours = Math.floor(minutes / 60);
+  const _minutes = minutes % 60;
+
+  return `${String(hours).padStart(2, '0')}:${String(_minutes).padStart(
+    2,
+    '0'
+  )}`;
+}
