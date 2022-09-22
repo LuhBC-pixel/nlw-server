3ae718c4aaf8b56596f71f881675b1bf2785f154
Luisa Boina Coltro <bc.luisa@yahoo.com.br>
2022-09-22 15:38:29 -0300

    add validation post

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-
+import { z, ZodError } from 'zod';
 import { PrismaClient } from '@prisma/client';
 import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes';
 import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string';
@@ -12,6 +12,16 @@ app.use(cors());
 
 const prisma = new PrismaClient({
   log: ['query'],
+});
+
+const createAdsSchema = z.object({
+  name: z.string(),
+  yearsPlaying: z.number(),
+  discord: z.string(),
+  weekDays: z.array(z.number()).min(0).max(6),
+  hourStart: z.string().regex(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/),
+  hourEnd: z.string().regex(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/),
+  useVoiceChannel: z.boolean(),
 });
 
 app.get('/games', async (reques, response) => {
@@ -32,20 +42,32 @@ app.post('/games/:id/ads', async (request, response) => {
   const gameId = request.params.id;
   const body: any = request.body;
 
-  const ad = await prisma.ad.create({
-    data: {
-      gameId,
-      name: body.name,
-      yearsPlaying: body.yearsPlaying,
-      discord: body.discord,
-      weekDays: body.weekDays.join(','),
-      hourStart: convertHourStringToMinutes(body.hourStart),
-      hourEnd: convertHourStringToMinutes(body.hourEnd),
-      useVoiceChannel: body.useVoiceChannel,
-    },
-  });
+  try {
+    createAdsSchema.parse(body);
+    const ad = await prisma.ad.create({
+      data: {
+        gameId,
+        name: body.name,
+        yearsPlaying: body.yearsPlaying,
+        discord: body.discord,
+        weekDays: body.weekDays.join(','),
+        hourStart: convertHourStringToMinutes(body.hourStart),
+        hourEnd: convertHourStringToMinutes(body.hourEnd),
+        useVoiceChannel: body.useVoiceChannel,
+      },
+    });
+    return response.status(201).json(ad);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return response.status(400).json({
+        message: 'Um dos campos deve estar com formatação errada',
+      });
+    }
 
-  return response.status(201).json(ad);
+    return response.status(500).json({
+      message: 'Something went wrong',
+    });
+  }
 });
 
 app.get('/games/:id/ads', async (request, response) => {
